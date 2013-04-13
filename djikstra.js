6f825463f5455e7e4907e240fa82e5f67736d181
cam <cam@cam-Inspiron-1564.(none)>
2013-04-13 13:11:26 -0600

    cleaning up

--- djikstra.js
+++ djikstra.js
@@ -1,25 +1,88 @@
 
 
 
-var testPath = function (nodes, start, destination, expected) {
-    var path = GetShortestPath(nodes, start, destination);
-    
-    if(!ArraysMatch(path, expected)) {
-        console.log('FAILED ' + start + ' to ' + destination);
-        console.log('Expected: ' + ObjectToString(expected));
-        console.log('Actual: ' + ObjectToString(path));
 
-    }
-    else {
-        console.log('PASSED ' + start + ' to ' + destination + ' ' + ObjectToString(path));
-    }
+var djikstra = function (nodes, start, destination) {
+
+    var path = []
+    var q = [];
+    q.push(start); 
+    nodes[start].visited = true;
+    nodes[start].distance = 0;
+
+    while(q.length > 0) {
     
+        var currentNode = nodes[removeShortest(nodes, q)]
+        
+        if(currentNode.item == destination) {   
+            while(currentNode.item != start) {
+                path.unshift(currentNode.previous.direction);
+                currentNode = nodes[currentNode.previous.item];
+            }
+            
+            break;
+        }
+        else {
+            for(var edge in currentNode.edges)
+            {
+                var neighbor = nodes[currentNode.edges[edge]]
+                
+                if(neighbor.visited == null) {
+                     var newDistance = currentNode.distance + 1;
+                    
+                    if(neighbor.distance == null || newDistance < neighbor.distance) {
+                        neighbor.distance = newDistance;
+                        neighbor.visited = true;
+                        q.push(neighbor.item);
+                        neighbor.previous = { 
+                            item: currentNode.item,
+                            direction: edge
+                        }
+                    }
+                }
+            }  
+        }
+    }
     
     clearNodes(nodes);
+    
+    return path;
 }
 
 
-var nodes = { 
+var removeShortest = function (nodes, arr) {
+    
+    var nodeWithShortestDistance;
+    var index;
+
+    for(var i = 0; i < arr.length; i++) {
+        var currentNode = nodes[arr[i]];
+        
+        if(nodeWithShortestDistance == null || 
+            currentNode.distance < nodeWithShortestDistance.distance) {
+            
+            nodeWithShortestDistance = currentNode;
+            index = i;
+        }
+    }
+
+    arr.splice(index,1);
+    
+    return nodeWithShortestDistance.item;
+}
+
+//TODO: store these breadcrumbs in a separate structure
+//to avoid having to clear the nodes
+var clearNodes = function (nodes) {
+
+    for(var node in nodes) {
+        nodes[node].visited = null;
+        nodes[node].distance = null;
+        nodes[node].previous = null;
+    }
+}
+
+var testNodes = { 
     "a" : { 
         item: "a" , 
         edges: {  
@@ -103,81 +166,6 @@ var nodes = {
     }       
 }
 
-
-var clearNodes = function () {
-
-    for(var node in nodes) {
-        nodes[node].visited = null
-        nodes[node].distance = null;
-        nodes[node].previous = null;
-    }
-}
-
-var removeShortest = function (arr) {
-    
-    var nodeWithShortestDistance;
-    var index;
-
-    for(var i = 0; i < arr.length; i++) {
-        var currentNode = nodes[arr[i]];
-        
-        if(nodeWithShortestDistance == null || 
-            currentNode.distance < nodeWithShortestDistance.distance) {
-            
-            nodeWithShortestDistance = currentNode;
-            index = i;
-        }
-    }
-
-    arr.splice(index,1);
-    
-    return nodeWithShortestDistance.item;
-}
+djikstra(testNodes, "a", "i");
 
 
-var getShortestPath = function (start, destination) {
-
-    var path = []
-    var q = [];
-    q.push(start); 
-    nodes[start].visited = true;
-    nodes[start].distance = 0;
-
-    while(q.length > 0) {
-    
-        var currentNode = nodes[removeShortest(q)]
-        
-        if(currentNode.item == destination) {   
-            while(currentNode.item != start) {
-                path.unshift(currentNode.previous.direction);
-                currentNode = nodes[currentNode.previous.item;
-            }
-            
-            break;
-        }
-        else {
-            for(var edge in currentNode.edges)
-            {
-                var neighbor = nodes[currentNode.edges[edge]]
-                
-                if(neighbor.visited == null) {
-                     var newDistance = currentNode.distance + 1;
-                    
-                    if(neighbor.distance == null || newDistance < neighbor.distance) {
-                        neighbor.distance = newDistance;
-                        neighbor.visited = true;
-                        q.push(neighbor.item);
-                        neighbor.previous = { 
-                            item: currentNode.item,
-                            direction: edge
-                        }
-                    }
-                }
-            }  
-        }
-    }
-    
-    clearNodes();
-    
-    return path;
-}
